ff3158d84d2081e31a30bade56a4407adc2a73c9
Chris Oliver <excid3@gmail.com>
2018-05-02 14:06:05 -0500

    Add generic dropdown Vue component using Slots

---
+++ app/javascript/components/dropdown.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-click-outside="clickedOutside" class="relative inline-block">
+
+    <div role="button" class="inline-block select-none" @click.prevent="open = !open">
+      <slot name="link"></slot>
+    </div>
+
+    <div v-show="open" class="absolute pin-r mt-2 z-10">
+      <slot name="dropdown-items"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    }
+  },
+
+  methods: {
+    clickedOutside() {
+      this.open = false
+    }
+  },
+}
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,37 @@
 // layout file, like app/views/layouts/application.html.erb
 
 import '../css/application.scss'
+
+import Vue from 'vue/dist/vue.esm'
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
+
+import Dropdown from '../components/dropdown'
+Vue.component('dropdown', Dropdown)
+
+// Directive for handling clicks outside of the element.
+// We use this to automatically close the dropdown if you
+// click somewhere else on the page.
+Vue.directive('click-outside', {
+  bind: function (el, binding, vNode) {
+    el.__vueClickOutside__ = event => {
+      if (!el.contains(event.target)) {
+        // call method provided in v-click-outside value
+        vNode.context[binding.expression](event)
+        event.stopPropagation()
+      }
+    }
+    document.body.addEventListener('click', el.__vueClickOutside__)
+  },
+  unbind: function (el, binding, vNode) {
+    // Remove Event Listeners
+    document.removeEventListener('click', el.__vueClickOutside__)
+    el.__vueClickOutside__ = null
+  }
+})
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: "[data-behavior='vue']",
+  })
+})
